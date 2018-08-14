e19650acaa08a98e456ecc276163c134dd98efc3
yaj <1252980880@qq.com>
2018-08-14 10:38:24 +0800

    add a script

---
+++ examples/computeLiNaMgAl.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+python3 batch_compute_Li.py > batch_com_Li_20180813.txt 2>&1
+python3 batch_compute_Na.py > batch_com_Na_20180813.txt 2>&1
+python3 batch_compute_Mg.py > batch_com_Mg_20180813.txt 2>&1
+python3 batch_compute_Al.py > batch_com_Al_20180813.txt 2>&1
