69198c0a27bc536398028c40a12236afe7fddbcf
yeanjiang <yeanjiang11@qq.com>
2019-09-18 22:24:17 +0800

    update

--- cavd/__init__.py
+++ cavd/__init__.py
@@ -204,7 +204,8 @@ def rediscovery_byRad_kdTree(stru, migrate, vorosites, vororad, threshold = 0.5)
         dis_st1_st2 = tmp_site1.distance(tmp_site2)
         migrate_mindis[str(migrate_labels[idx])] = (pt_tag, pt_rad, dis_st1_st2)
         
-        if dis_st1_st2 <= threshold or dis_st1_st2 <= pt_rad:
+        # if dis_st1_st2 <= threshold or dis_st1_st2 <= pt_rad:
+        if dis_st1_st2 <= threshold:
             recover_state[str(migrate_labels[idx])] = pt_tag
             recover_labels.append(migrate_labels[idx])
         else:
